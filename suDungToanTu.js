6febe1b5466cc0cd1ef75551d18223882d45ccd5
Ngô Minh Hiếu <hieunm92@gmail.com>
2020-11-10 14:03:41 +0700

    Thuc hanh

---
+++ suDungToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Chiều dài: ");
+inputHeight = prompt("Chiều rộng:");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích: " + area);
